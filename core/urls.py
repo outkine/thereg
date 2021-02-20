988cc1057c5651b07fdf86a18b0603961b07238c
Anton <antonoutkine@gmail.com>
2021-02-20 11:18:15 -0600

    Project 3
    Login/logout and usability improvements

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('users/<str:username>', views.user, name='user'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
