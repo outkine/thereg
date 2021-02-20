a4a7a7fc39652045ac843bd034a89e99c67c3af7
Anton <antonoutkine@gmail.com>
2021-02-20 10:48:50 -0600

    Project 2 Challenge 1
    List users on index page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import User
+
 def index(request):
-    return render(request, 'index.html')
+    users = User.objects.all()
+    return render(request, 'index.html', {'users': users})
 
