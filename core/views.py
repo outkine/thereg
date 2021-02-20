0e557f0240171e400d8625a6453c536d809291a1
Anton <antonoutkine@gmail.com>
2021-02-20 10:55:23 -0600

    Project 2 Challenge 2
    Add user profile page

--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import User
 
 def index(request):
     users = User.objects.all()
     return render(request, 'index.html', {'users': users})
+
+def user(request, username):
+    user = get_object_or_404(User, username=username)
+    return render(request, 'user.html', {'user': user})
+
 
